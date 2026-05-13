ea240a95b8a2170637920c9594417b93d4d4496e
poomichopoom-p <Poomicho.p@gmail.com>
2026-05-13 15:14:12 +0700

    add-2-api

---
+++ src/fakeData/fakeUsers.js
@@ -0,0 +1,27 @@
+export const users = [
+  {
+    id: "1",
+    username: "Neeti",
+    email: "neeti@example.com",
+    password: "1234",
+  },
+  {
+    id: "2",
+    username: "john",
+    email: "John@example.com",
+    password: "5566777",
+  },
+  {
+    id: "3",
+    username: "peter",
+    email: "peter@example.com",
+    password: "3453745",
+  },
+  {
+    id: "4",
+    username: "alice",
+    email: "alice@example.com",
+    password: "657965667",
+  },
+];
+
