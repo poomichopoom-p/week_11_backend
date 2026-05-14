cbb86c27e58184284aedb9e34fb2178d0f93e1ee
poomichopoom-p <Poomicho.p@gmail.com>
2026-05-14 13:23:17 +0700

    CRUD for user resouce

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 import express from "express";
+import cors from "cors";
+
 import { users } from "./fakeData/fakeUsers.js";
-
-
-
 const app = express();
-
+app.use(cors());
+app.use(express.json());
 const port = 3000;
+
+//  let users = [
+//   {
+//     id: "1",
+//     username: "poom",
+//     email: "poom@gmail.com",
+//     password: "1234",
+//   },
+// ];
 
 app.get("/", (req, res) => {
   res.send(`<!doctype html>
@@ -42,6 +51,60 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   res.json(users);
+});
+app.post("/users", (req, res) => {
+  const { username, email } = req.body || {};
+  if (!username || !email) {
+    return res.status(400).json({ error: "username and email are required" });
+  }
+
+  const nextId = String(
+    (users.reduce((max, u) => Math.max(max, Number(u.id)), 0) || 0) + 1,
+  );
+
+
+  const newUser = { id: nextId, username, email };
+
+  users.push(newUser);
+
+  return res.status(201).json(newUser);
+});
+
+// app.post("/createAccount", (req, res) => {
+//   const newUser = {
+//     id : DataTransfer.now().toString(),
+//     username : req.body.username,
+//     email : req.body.email,
+//     password :req.body.password,
+//   };
+//   users.push(newUser);
+
+//   res.status(201).json({
+//     message:"User created",
+//     data: newUser,
+//   });
+// });
+// app.delete("/deleteAccount", (req, res) => {});
+app.put("/users/:id", (req, res) => {
+
+ const user = users.find((u) => u.id === req.params.id);
+  
+  if(!user){
+    return res.status(404).json({error: "User not found!"})
+  }
+
+const {username, email, password} = req.body
+
+  if(!username || !email || !password){
+    return res.status(400).json({error:"username email and password not found!"})
+  }
+
+  user.username = username;
+  user.email = email;
+  user.password =password;
+
+  res.status(200).json(user);
+
 });
 
 app.listen(port, () => {
